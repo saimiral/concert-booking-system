79ee606038626689901e90009d13beda9809cfba
Saimir Alla <allasaimir44@gmail.com>
2026-03-06 00:25:32 +0200

    Initial commit - Spring Boot setup

---
+++ concert-booking-system/src/main/java/com/saimiral/concert_booking_system/ConcertBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.saimiral.concert_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertBookingSystemApplication.class, args);
+	}
+
+}
---
+++ concert-booking-system/src/test/java/com/saimiral/concert_booking_system/ConcertBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saimiral.concert_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
