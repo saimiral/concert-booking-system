bd07a82fdacf0d421398bedb616f1864864f7141
Saimir Alla <allasaimir44@gmail.com>
2026-03-19 23:30:56 +0200

    Add CORS configuration to controllers

--- src/main/java/com/saimiral/concert_booking_system/controller/ConcertController.java
+++ src/main/java/com/saimiral/concert_booking_system/controller/ConcertController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/concerts")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ConcertController {
     private final ConcertService service;
 
--- src/main/java/com/saimiral/concert_booking_system/controller/ReservationController.java
+++ src/main/java/com/saimiral/concert_booking_system/controller/ReservationController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/reservations")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ReservationController {
     private final ReservationService reservationService;
 
